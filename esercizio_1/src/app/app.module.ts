077efa3fd1a249bfc015d9a5dc1f06a5e8f031fb
madsgit0 <landolfomattia@gmail.com>
2022-11-03 20:34:12 +0100

    .

---
+++ esercizio_1/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginPage } from './login/login.page';
+import { SignupPage } from './signup/signup.page';
+import { TodolistPage } from './todolist/todolist.page';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPage
+  },{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },{
+    path: 'signup',
+    component: SignupPage
+  },{
+    path: 'todolist',
+    component: TodolistPage
+  },
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPage,
+    SignupPage,
+    TodolistPage,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ esercizio_1/src/app/todolist/todolist.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './todolist.page.html',
+  styleUrls: ['./todolist.page.css']
+})
+export class TodolistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
