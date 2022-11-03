ecf2483c12b4c8611255677dc525e0e30716f00b
madsgit0 <landolfomattia@gmail.com>
2022-11-04 00:04:58 +0100

    .

--- esercizio_1/src/app/todolist/todolist.page.ts
+++ esercizio_1/src/app/todolist/todolist.page.ts
@@ -6,9 +6,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistPage implements OnInit {
 
+  todos = [{
+    id: 1,
+    text: "testo di prova",
+    check: "",
+  },{
+    id: 2,
+    text: "testo di ",
+    check: "check",
+  },{
+    id: 3,
+    text: "abc",
+    check: "",
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ removeTodo(i: number){
+  this.todos.splice(i,1);
+ }
+ checkTodo(i: number){
+  this.todos[i].check = this.todos[i].check === "check" ? "" : "check";
+ }
 }
